407005d3ababae4efc822665376958068cb0fe3d
Uny1me <cornellkid2@gmail.com>
2021-08-01 21:19:43 +0100

    Main fixes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,15 +5,17 @@ import BrandLogo from '../images/logo_mark.png'
 class Nav extends Component {
     render() {
         return (
-            <Nav>
+            <div>
                 <Navbar.Brand href="#home" className='d-flex p-4'>
-                <img src={BrandLogo}
-                    width="30"
-                    height="30"
-                    className="align-top"
-                    alt="Company Logo"
-                /> <p className='p-3 pt-0 pb-0 text-dark info'>Your Cart Page</p> </Navbar.Brand>
-            </Nav>
+                    <img src={BrandLogo}
+                        width="30"
+                        height="30"
+                        className="align-top"
+                        alt="Company Logo"
+                    />
+                    <p className='p-3 pt-0 pb-0 text-dark info'>Your Cart Page</p>
+                </Navbar.Brand>
+            </div>
         );
     }
 }
